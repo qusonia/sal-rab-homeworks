8dae4307af5f28772be260b6be1f26eebb75d8e5
shabanovavv1997 <122607714+shabanovavv1997@users.noreply.github.com>
2023-02-22 16:09:25 +0300

    Update solution.js

--- task_2/solution.js
+++ task_2/solution.js
@@ -10,8 +10,7 @@ function calcShipping(sum, min, shipping) {
     if (productsSum == 0 || productsSum >= freeShippingMinSum) {
         shippingSum = 0;
     }
-    else ((productsSum > 0) && (productsSum < freeShippingMinSum)) 
-                shippingSum = shippingPrice ;
+    
 
     // Конец решения задания №2.1.
 

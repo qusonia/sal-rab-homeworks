29c28b28a819d767bb08fc0df5dc2fdce1658198
shabanovavv1997 <122607714+shabanovavv1997@users.noreply.github.com>
2023-03-04 10:23:18 +0300

    Update solution.js

--- task_3/solution.js
+++ task_3/solution.js
@@ -4,11 +4,11 @@ function renderCartItem(item) {
     // Задание №3.1. Формирование строки корзины
 
     product = {id: id, title: title, count: count, price: price};
-    let itemCountText = '';
+    let itemCountText ='';
     itemCountText = itemCountText + product.count;
-    itemCountText = itemCountText +' × ';
+    itemCountText = itemCountText +'×';
     itemCountText = itemCountText + product.price;
-    itemCountText = itemCountText +'₽='  ; 
+    itemCountText = itemCountText +'₽='; 
     let sum = product.count * product.price;
     itemCountText = itemCountText + sum; 
     itemCountText = itemCountText +’₽’;

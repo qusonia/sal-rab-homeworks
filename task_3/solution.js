fbf115fb04f92a75c87b7a9f01a39a7c9eff79f8
shabanovavv1997 <122607714+shabanovavv1997@users.noreply.github.com>
2023-03-10 14:22:08 +0300

    Update solution.js

--- task_3/solution.js
+++ task_3/solution.js
@@ -2,7 +2,7 @@ function renderCartItem(item) {
    const product = item; 
     // Задание №3.1. Формирование строки корзины
 
-    product = {id: 1, title: 'Пицца', count: 5, price: '500.00'};
+    product = {id: id, title: title, count: count, price: price};
     let itemCountText ='';
     itemCountText = itemCountText + product.count;
     itemCountText = itemCountText +' × ';
